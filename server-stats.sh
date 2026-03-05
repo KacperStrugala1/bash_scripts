fb6940051e8923a35cff94239f295b2631666128
Kacper Strugala <108838288+KacperStrugala1@users.noreply.github.com>
2026-03-05 19:58:49 +0100

    Add server-stats.sh for monitoring system resources
    This script retrieves and displays CPU, RAM, and disk usage statistics.

---
+++ server-stats.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CPU_USAGE=$(top -n2 -b | grep "%Cpu" | tail -1 | awk '{print 100-$8}')
+RAM_USAGE=$(top -n2 -b | grep "MiB Mem" | tail -1 | awk '{print $8/$4*100}')
+RAM_AVAILABLE=$(top -n2 -b | grep "MiB Mem" | tail -1 | awk '{print 100-($8/$4*100)}')
+DISK_AVAILABLE=$(df -h | grep "C:" | awk '{print $4}')
+DISK_USED=$(df -h | grep "C:" | awk '{print $3}')
+
+echo "CPU USAGE:" $CPU_USAGE "% "
+echo "RAM USAGE:" $RAM_USAGE "%"
+echo "RAM AVAILABLE:" $RAM_AVAILABLE "%"
+echo "USED DISK SPACE:" $DISK_USED"B"
+echo "AVAILABLE DISK SPACE:" $DISK_AVAILABLE"B"
